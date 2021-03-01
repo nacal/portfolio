88fdba0681787e5eafa67f8eb35829113dedd0ab
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-02 06:33:49 +0900

    Develop default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="bg-primary">
-    <Nuxt />
+  <div class="bg-base">
+    <Header />
+    <GlobalNav class="transform translate-x-64 lg:translate-x-0" />
+    <main class="mt-24 lg:mr-64">
+      <Nuxt />
+    </main>
   </div>
 </template>
+
+<script>
+import Header from '../components/layout/Header'
+import GlobalNav from '../components/layout/GlobalNav'
+
+export default {
+  components: {
+    Header,
+    GlobalNav,
+  },
+}
+</script>
+
+<style lang="postcss">
+.flex-center {
+  @apply items-center justify-center;
+}
+</style>

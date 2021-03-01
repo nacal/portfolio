d16e7888a39a58cbd67f976603b324f99f0d00bd
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-02 06:32:49 +0900

    Develop header layout

---
+++ components/layout/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="bg-primary h-24 fixed w-full top-0">
+    <div class="w-full h-full px-4 flex items-center justify-between">
+      <h1 class="text-white text-2xl font-extralight tracking-widest">
+        nkthkr portfolio.
+      </h1>
+      <Hamburger :open="isOpen" />
+    </div>
+  </header>
+</template>
+
+<script>
+import Hamburger from '../Hamburger.vue'
+
+export default {
+  components: { Hamburger },
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+}
+</script>

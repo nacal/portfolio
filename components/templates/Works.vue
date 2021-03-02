4d190d234c98b3ad5dbe984502f9a09a6671d56a
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-03 06:38:55 +0900

    Develop works section

---
+++ components/templates/Works.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="works" class="section">
+    <Title title="works" />
+    <div class="grid gap-16 grid-col-fit max-w-full">
+      <Card v-for="card in 10" :key="card" />
+    </div>
+  </section>
+</template>
+
+<script>
+import Title from '@/components/atoms/Title'
+import Card from '@/components/atoms/Card'
+
+export default {
+  components: {
+    Title,
+    Card,
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.grid-col-fit {
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div class="container">
     <Hero />
     <Profile />
+    <Works />
   </div>
 </template>
 
 <script>
 import Hero from '@/components/templates/Hero'
 import Profile from '@/components/templates/Profile'
+import Works from '@/components/templates/Works'
 
 export default {
   components: {
     Hero,
     Profile,
+    Works,
   },
 }
 </script>

495d7cc39440a7d4c1426c4e25f679b8d722bff9
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-03 06:38:44 +0900

    Develop Card component

---
+++ components/atoms/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <section
+    class="w-96 max-w-full rounded overflow-hidden shadow-lg my-2 mx-auto"
+  >
+    <img
+      class="w-full"
+      src="https://tailwindcss.com/img/card-top.jpg"
+      alt="Sunset in the mountains"
+    />
+    <div class="px-6 py-4 bg-base">
+      <h1 class="font-bold text-xl mb-2">NKTech</h1>
+      <p class="text-secondary text-sm">JamStackな構成の高速なブログ</p>
+    </div>
+    <div class="px-6 py-4 bg-base text-sm flex flex-wrap">
+      <span class="mr-2">#Vue.js(Nuxt.js)</span>
+      <span class="mr-2">#Contentful</span>
+      <span class="mr-2">#Netlify</span>
+      <span class="mr-2">#Netlify</span>
+      <span class="mr-2">#Netlify</span>
+      <span>#Netlify</span>
+    </div>
+    <div class="bg-accent flex text-center bg-accent">
+      <span class="w-1/2 h-full py-2 text-sm border-r border-secondary"
+        >Code</span
+      >
+      <span class="w-1/2 h-full py-2 text-sm">Visit</span>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>

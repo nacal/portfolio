e5a2153c3b71f77394f50669a2b0931506d6c83f
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-03 05:26:28 +0900

    Fix overlay

--- components/organisms/Header.vue
+++ components/organisms/Header.vue
@@ -1,7 +1,10 @@
 <template>
   <header class="bg-primary h-24 fixed w-full top-0">
     <div class="w-full h-full px-4 lg:px-8 flex items-center justify-between">
-      <h1 class="text-white text-2xl font-extralight tracking-widest">
+      <h1
+        class="text-white text-2xl font-extralight tracking-widest"
+        @click="close()"
+      >
         <nuxt-link v-scroll-to="'#hero'" to>nkthkr portfolio.</nuxt-link>
       </h1>
       <Hamburger :open="isOpen" />
@@ -10,6 +13,7 @@
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 import Hamburger from '@/components/atoms/Hamburger'
 
 export default {
@@ -18,6 +22,11 @@ export default {
     return {
       isOpen: false,
     }
+  },
+  methods: {
+    ...mapMutations({
+      close: 'close',
+    }),
   },
 }
 </script>

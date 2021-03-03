5e651137a1ab724891176d5f8a99523b58ca0f1d
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-04 03:14:43 +0900

    Fix profile card layout

--- components/atoms/ProfileCard.vue
+++ components/atoms/ProfileCard.vue
@@ -1,50 +1,52 @@
 <template>
   <div
-    class="max-w-full mx-auto bg-gray-800 shadow-2xl rounded-lg overflow-hidden"
+    class="max-w-full mx-auto bg-gray-800 shadow-2xl rounded-lg overflow-hidden sm:pr-16"
   >
     <div class="py-3 px-4 flex">
       <div class="rounded-full w-3 h-3 bg-red-500 mr-2"></div>
       <div class="rounded-full w-3 h-3 bg-yellow-500 mr-2"></div>
       <div class="rounded-full w-3 h-3 bg-green-500"></div>
     </div>
-    <div class="py-4 px-4 mt-1 text-white">
+    <div class="py-4 px-4 mt-1 text-white text-xs md:text-base">
       <div class="mb-2">
         <span class="text-yellow-300">data ( )</span>
         <span class="ml-2">{</span>
       </div>
 
-      <div class="mb-2 ml-4">
+      <div class="mb-2 ml-2 md:ml-4">
         <span class="text-yellow-600">return</span>
         <span class="ml-2">{</span>
       </div>
 
-      <div class="mb-2 ml-8">
-        <span class="text-blue-300">name</span>
+      <div class="mb-2 ml-4 md:ml-8">
+        <span class="text-blue-300">Name</span>
         <span class="mr-2">:</span>
-        <span class="text-green-300">'中田 輝(Hikaru Nakata)'</span>
+        <span class="text-green-300">' Hikaru Nakata '</span>
         <span>,</span>
       </div>
 
-      <div class="mb-2 ml-8">
-        <span class="text-blue-300">school</span>
+      <div class="mb-2 ml-4 md:ml-8">
+        <span class="text-blue-300">School</span>
         <span class="mr-2">:</span>
-        <span class="text-green-300">'同志社大学大学院(M1)'</span>
+        <span class="text-green-300">' Doshisha Univ Grad School '</span>
         <span>,</span>
       </div>
 
-      <div class="mb-2 ml-8">
-        <span class="text-blue-300">skills</span>
+      <div class="mb-2 ml-4 md:ml-8">
+        <span class="text-blue-300">DateOfBirth</span>
         <span class="mr-2">:</span>
-        <span>[</span>
-        <span class="text-green-300">'HTML/CSS'</span>
+        <span class="text-green-300">' 1997-7-17 '</span>
         <span>,</span>
-        <span class="text-green-300">'Vue.js(Nuxt.js)'</span>
-        <span>,</span>
-        <span class="text-green-300">'Vue.js(Nuxt.js)'</span>
-        <span>, ... ] ,</span>
       </div>
 
-      <div class="mb-2 ml-4">
+      <div class="mb-2 ml-4 md:ml-8">
+        <span class="text-blue-300">Skill</span>
+        <span class="mr-2">:</span>
+        <span class="text-green-300">' Frontend '</span>
+        <span>,</span>
+      </div>
+
+      <div class="mb-2 ml-2 md:ml-4">
         <span>}</span>
       </div>
 

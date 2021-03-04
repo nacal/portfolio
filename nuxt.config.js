403d95177d91bcc7ef337f0ebe1366296cb0b48f
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2021-03-04 09:05:53 +0900

    Setting

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,9 +8,62 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: "Hikaru Nakata's portfolio",
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'nkthkr portfolio',
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'サイトのURL' },
+      { hid: 'og:title', property: 'og:title', content: 'nkthkr portfolio' },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: "Hikaru Nakata's portfolio",
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: '/portfolio-card.png',
+      },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'msapplication-TileColor', content: '#99b5ad' },
+      { name: 'theme-color', content: '#ffffff' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/favicon/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon/favicon-16x16.png',
+      },
+      {
+        rel: 'manifest',
+        href: '/favicon/site.webmanifest',
+      },
+      {
+        rel: 'mask-icon',
+        href: '/favicon/safari-pinned-tab.svg',
+        color: '#5bbad5',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
